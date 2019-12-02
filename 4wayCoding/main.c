d6521d87bfc06e83799f88ae950419f06ad7a2f5
Aidan Rosen <aidanrosen04@gmail.com>
2019-12-02 14:10:46 -0800

    Initial Commit

---
+++ 4wayCoding/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  4wayCoding
+//
+//  Created by Rosen, Aidan on 12/2/19.
+//  Copyright © 2019 Rosen, Aidan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
